c975406c1ea7f32ade0e9dac55a0440b07e950cf
uemuram <uemura.masaru@tis.co.jp>
2019-04-21 17:18:52 +0900

    種類別に階層を変更

--- src/main/java/com/example/demo/controller/HelloSpringBootWebController.java
+++ src/main/java/com/example/demo/controller/HelloSpringBootWebController.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package com.example.demo.controller;
 
 import org.springframework.stereotype.Controller;
 
@@ -6,6 +6,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
+
+import com.example.demo.service.HelloSpringBootWebService;
+
 import org.springframework.beans.factory.annotation.Autowired;
 
 @Controller
