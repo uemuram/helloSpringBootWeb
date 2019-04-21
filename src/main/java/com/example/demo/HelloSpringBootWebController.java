20ac9ec4f890c2cedc7345f630311a94913ffebd
uemuram <uemura.masaru@tis.co.jp>
2019-04-21 16:42:11 +0900

    サービスクラス呼び出しを追加

--- src/main/java/com/example/demo/HelloSpringBootWebController.java
+++ src/main/java/com/example/demo/HelloSpringBootWebController.java
@@ -1,22 +1,34 @@
 package com.example.demo;
+
 import org.springframework.stereotype.Controller;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.beans.factory.annotation.Autowired;
 
 @Controller
 public class HelloSpringBootWebController {
-    @RequestMapping(value="/", method=RequestMethod.GET)
-    public ModelAndView index(ModelAndView mv) {
-        mv.setViewName("index");
-        return mv;
-    }
 
-    @RequestMapping(value="/result", method=RequestMethod.POST)
-    public ModelAndView send(@RequestParam("inputvalue")String inputvalue, ModelAndView mv) {
-        mv.setViewName("result");
-        mv.addObject("message", inputvalue);
-        return mv;
-    }
+	// DIされる
+	@Autowired
+	HelloSpringBootWebService service;
+
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public ModelAndView index(ModelAndView mv) {
+		mv.setViewName("index");
+		return mv;
+	}
+
+	@RequestMapping(value = "/result", method = RequestMethod.POST)
+	public ModelAndView send(@RequestParam("inputvalue") String inputvalue, ModelAndView mv) {
+
+		// DIされたサービス呼び出し
+		int x = service.doSomeService();
+
+		mv.setViewName("result");
+		mv.addObject("message", inputvalue + x);
+		return mv;
+	}
 }
---
+++ src/main/java/com/example/demo/HelloSpringBootWebService.java
@@ -0,0 +1,10 @@
+package com.example.demo;
+import org.springframework.stereotype.Service;
+
+@Service
+public class HelloSpringBootWebService {
+
+	public int doSomeService() {
+		return 3;
+	}
+}
